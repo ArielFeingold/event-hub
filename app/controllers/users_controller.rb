f4b91a117005505b58ab00b8dc5791a593b8a1dc
ArielFeingold <feingold.ariel@gmail.com>
2018-05-03 12:39:24 -0400

    added create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -12,4 +19,13 @@ class UsersController < ApplicationController
   def show
   end
 
+  private
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :password,
+        :about,
+        )
+  end
 end
