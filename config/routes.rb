fe1348f00acc35157d8e72a73e4032613dbe651c
ArielFeingold <feingold.ariel@gmail.com>
2018-05-03 11:58:44 -0400

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/destroy'
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
   delete '/session/', to: 'session#destroy'
