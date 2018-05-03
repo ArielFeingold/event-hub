87a45412254ce64503e9e8e727765aee7cc47aa8
ArielFeingold <feingold.ariel@gmail.com>
2018-05-03 13:21:08 -0400

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show, :edit, :update]
 
+  root 'static_pages#home'
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
   delete '/session/', to: 'session#destroy'
