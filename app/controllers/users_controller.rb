04d9303bfa98a0a8d4df71a58dc1296eae7bc93e
ArielFeingold <feingold.ariel@gmail.com>
2018-05-03 12:07:22 -0400

    users controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,4 @@ class UsersController < ApplicationController
   def show
   end
 
-  def destroy
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/destroy'
+  resources :users, only: [:new, :create, :show, :edit, :update]
+
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
   delete '/session/', to: 'session#destroy'
