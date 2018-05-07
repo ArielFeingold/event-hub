53e142e6f2a5f42dbadd01fc3639c80c35a255be
ArielFeingold <feingold.ariel@gmail.com>
2018-05-07 11:46:15 -0400

    Changed user table to streamline with facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :update]
 
   root 'static_pages#home'
+  get '/auth/facebook/callback' => 'session#create'
 
   controller :session do
     get  'login' => :new
--- db/migrate/20180503131430_create_users.rb
+++ db/migrate/20180503131430_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
       t.string :email
+      t.string :uid
       t.string :password_digest
-      t.string :first_name
-      t.string :last_name
       t.text :about
+      t.string :image
+
 
       t.timestamps
     end
